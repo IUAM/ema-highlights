99c2e07d32a9e6d12d73b36ed5e2143b6de06cb5
Illya Moskvin <ivmoskvin@gmail.com>
2018-08-19 12:05:39 -0500

    Port code to app.js from old project
    Using `copy` instead of `js` because we don't need webpack magic here.

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,5 +13,5 @@ let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/vendor.js', 'public/js')
    .sass('resources/assets/sass/vendor.scss', 'public/css')
-   .js('resources/assets/js/app.js', 'public/js')
+   .copy('resources/assets/js/app.js', 'public/js/app.js')
    .sass('resources/assets/sass/app.scss', 'public/css');
