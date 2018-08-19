c538b3c0b539a8e971605bc60b6b17868527487b
Illya Moskvin <ivmoskvin@gmail.com>
2018-08-19 12:02:02 -0500

    Add fancybox@2.1.5

--- resources/assets/js/vendor.js
+++ resources/assets/js/vendor.js
@@ -2,10 +2,9 @@
  * Require dependencies from npm
  */
 
-try {
-    window.$ = window.jQuery = require('jquery');
+window.$ = window.jQuery = require('jquery');
 
-    // Uncomment if the site needs Bootstrap's dynamic components
-    // require('bootstrap');
-} catch (e) {}
+// Uncomment if the site needs Bootstrap's dynamic components
+// require('bootstrap');
 
+require('fancybox/dist/js/jquery.fancybox')($);
