6dff059036f7cfffa12ec55c7f1bd768dde9df89
Illya Moskvin <ivmoskvin@gmail.com>
2018-08-19 00:31:32 -0500

    Resolve frontend dependencies
    This setup roughly matches that which was used for the non-Laravel
    version of this project. I'd like to separate built app files from
    vendor ones.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,22 +1,11 @@
-
 /**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
+ * Require dependencies from npm
  */
 
-require('./bootstrap');
+try {
+    window.$ = window.jQuery = require('jquery');
 
-window.Vue = require('vue');
+    // Uncomment if the site needs Bootstrap's dynamic components
+    // require('bootstrap');
+} catch (e) {}
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
-const app = new Vue({
-    el: '#app'
-});
