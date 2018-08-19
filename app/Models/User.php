ebf0b6d123a18e36552944af68cbfcc24a9fb787
Illya Moskvin <ivmoskvin@gmail.com>
2018-08-18 21:56:36 -0500

    Remove ability to reset passwords
    We'll be relying exclusively on IU's CAS for authentication, so this is
    cruft within the context of our implementation.

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,21 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Auth\Authenticatable;
-use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
-use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 use Illuminate\Notifications\Notifiable;
 
 use Illuminate\Database\Eloquent\Model;
 
 class User extends Model implements
     AuthenticatableContract,
-    AuthorizableContract,
-    CanResetPasswordContract
+    AuthorizableContract
 {
-    use Authenticatable, Authorizable, CanResetPassword, Notifiable;
+    use Authenticatable, Authorizable, Notifiable;
 
     /**
      * The attributes that are mass assignable.
