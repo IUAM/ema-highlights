d09a103ccc5da3e526b39f5bab8f3c249edc1df1
Illya Moskvin <ivmoskvin@gmail.com>
2018-09-23 19:45:37 -0500

    Adapt layout template from old website

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,71 +4,110 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-site-verification" content="kMzbIa-aqIRJrt7Rs0qRWka7a0bEsmyg-rG25il3ND0" />
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <!-- For migrating servers - IE needs http:// -->
+    <base href="{!! config('app.url') !!}" />
+
     <!-- Scripts -->
+    <script src="{{ asset('js/vendor.js') }}" defer></script>
     <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Lato:300,400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.iu.edu/style.css?family=BentonSans:regular,bold|BentonSansCond:regular" media="screen" rel="stylesheet" type="text/css"/>
 
     <!-- Styles -->
+    <link href="{{ asset('css/vendor.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- Google Analytics -->
+    <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-89717795-1', 'auto');
+    ga('send', 'pageview');
+    </script>
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
+<div id="branding-bar">
+    <img src="images/trident-large.png" alt="IU">
+    <div id="branding-row">
+        <p id="iu-campus">
+            <a href="https://www.indiana.edu" title="Indiana University Bloomington">
+                <span class="show-on-desktop" itemprop="name">Indiana University Bloomington</span>
+                <span class="show-on-tablet" itemprop="name">IU Bloomington</span>
+                <span class="show-on-mobile" itemprop="name">IUB</span>
+            </a>
+        </p>
     </div>
+</div>
+
+<div id="sidebar">
+
+    <div id="header">
+
+        <a id="logo" href="https://artmuseum.indiana.edu">
+            <img src="images/ema-logo-white.png" />
+        </a>
+
+        <a id="hamburger" href="javascript:;" class="btn-mobile">
+            <span class="glyphicon glyphicon-th-list"></span>
+        </a>
+
+    </div>
+
+    <form id="search" action="view/search" method="get">
+        <div class="input-group">
+            <input type="text" class="form-control input-sm" placeholder="Search the collection" name="q">
+            <span class="input-group-btn">
+                <button class="btn btn-sm" type="submit" id="search-submit"><span class="glyphicon glyphicon-search"></span></button>
+            </span>
+        </div>
+    </form>
+
+    @if (isset($sidebar))
+    <div id="menu" class="menu menu-sidebar">
+        <ul>
+            <li>
+                <a href="view/rights">Rights & Reproductions</a>
+            </li>
+        </ul>
+    </div>
+    @endif
+
+</div>
+
+<main id="main">
+    @yield('content')
+</main>
+
+<footer id="footer">
+
+    <p class="tagline">Fulfilling <span>the</span> Promise</p>
+
+    <p class="signature">
+        <a href="https://www.iu.edu" class="signature-link">Indiana University</a>
+    </p>
+
+    <p class="copyright">
+    <span class="line-break-small"><a href="https://www.iu.edu/copyright/index.html">Copyright</a> &#169; 2016</span> <span class="line-break-small">The Trustees of <a href="https://www.iu.edu/" itemprop="url"><span itemprop="name">Indiana University</span></a></span><span class="hide-on-mobile"> | </span><span class="line-break"><a href="https://artmuseum.indiana.edu/privacy" id="privacy-policy-link">Privacy Notice</a> | <a href="https://accessibility.iu.edu" id="accessibility-link" title="Having trouble accessing this web page because of a disability? Visit this page for assistance.">Accessibility</a></span>
+    </p>
+
+</footer>
+
+<!-- Litmus test for mobile screens -->
+<div id="mobile"></div>
+
 </body>
 </html>
