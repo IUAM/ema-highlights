3acb2d07f0a094604edf82fa11d41ac3f9d1b19d
Illya Moskvin <ivmoskvin@gmail.com>
2018-09-23 19:31:34 -0500

    Add safe length-based title getters to categories
    Will be necessary for sidebar.

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -7,8 +7,29 @@ use App\Models\Entry;
 class Category extends AbstractModel
 {
 
+    protected $appends = [
+        'title_medium_safe',
+        'title_short_safe',
+        'title_abbr_safe',
+    ];
+
     public function entries() {
         return $this->belongsToMany(Entry::class, 'category_entry')->withPivot('weight');
+    }
+
+    public function getTitleMediumSafeAttribute()
+    {
+        return $this->title_medium ?? $this->title;
+    }
+
+    public function getTitleShortSafeAttribute()
+    {
+        return $this->title_short ?? $this->title_medium_safe;
+    }
+
+    public function getTitleAbbrSafeAttribute()
+    {
+        return $this->title_abbr ?? $this->title_short_safe;
     }
 
 }
